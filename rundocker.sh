43d0fdd1af5444d9b5191463360969f53684ba12
jere-mie <jere-mie@users.noreply.github.com>
2023-09-21 00:06:03 -0400

    fix linux docker script

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker pull jeremiedevelops/easy-masm:latest
-docker run --rm -v ./src:/easy-masm/src/ jeremiedevelops/easy-masm:latest $1
+docker run --rm -v ./src:/easy-masm/src/ jeremiedevelops/easy-masm:latest sh run.sh $1
